fc12a734f429d0fa5a5a41657b6319df89debe96
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-01-27 17:24:18 +0530

    added new components and fetch notes method

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import RateLimited from "../components/RateLimited";
+import axios from "axios";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes/")
+
+        console.log(res)
+      } catch(error) {
+        console.log("Error fetching notes", error)
+      }
+    }
+
+    fetchNotes();
+  }, [])
+
   return (
-    <div>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div className="min-h-screen">
+      <NavBar />
+      {isRateLimited && <RateLimited />}
     </div>
   )
 }
