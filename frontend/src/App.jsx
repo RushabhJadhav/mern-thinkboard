54ec999c5192eea648c81b95f2a58d3d6bb842ac
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-01-27 16:25:28 +0530

    added themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,9 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={HomePage}></Route>
-        <Route path='/create' element={CreatePage}></Route>
-        <Route path='/note/id' element={NoteDetailPage}></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/create' element={<CreatePage />}></Route>
+        <Route path='/note/id' element={<NoteDetailPage />}></Route>
       </Routes>
     </div>
   )
