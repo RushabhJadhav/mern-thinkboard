e96e87f20301a6f4cbf593fac6629c2aefc22c26
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-01-30 19:52:46 +0530

    Updated note detail page

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,7 +1,121 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState({ title: '', content: '' });
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.log(error);
+        toast.error("Failed to fetch the note");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchNote();
+  }, [id]);
+
+  const handleSave = async () => {
+    if(!note.title.trim() || !note.content.trim()) {
+      toast.error("Please add a title or content");
+      return;
+    }
+
+    setSaving(true);
+
+    try {
+      const res = await api.put(`/notes/${id}`, note);
+      toast.success("Note updated successfully");
+      console.log(res);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to save the note");
+    } finally {
+      setSaving(false);
+    }
+  };
+  
+  const handleDelete = async () => {
+    if(!window.confirm("Are you sure you want to delete this note?")) return;
+  
+    try {
+      const res = await api.delete(`/notes/${id}`);
+      toast.success("Note deleted");
+      console.log(res);
+      navigate("/")
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete the note");
+    }
+  };
+
   return (
-    <div>NoteDetailPage</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to="/" className="btn btn-ghost">
+              <ArrowLeftIcon className="h-5 w-5" />
+              Back to Notes
+            </Link>
+            <button onClick={handleDelete} className="btn btn-error btn-outline">
+              <Trash2Icon />
+              Delete Note
+            </button>
+          </div>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mb-4">
+                <label className="label mb-2">
+                  <span className="label-text">Title</span>
+                </label>
+                <input type="text"
+                  placeholder="Note Title"
+                  className="input input-bordered w-full max-w-none p-4"
+                  value={note.title}
+                  onChange={e => setNote({ ...note, title: e.target.value })}
+                />
+              </div>
+
+              <div className="form-control mb-4">
+                <label className="label mb-2">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea
+                  placeholder="Write your note here..."
+                  className="textarea textarea-bordered h-32 w-full max-w-none p-4"
+                  value={note.content}
+                  onChange={e => setNote({ ...note, content: e.target.value })}
+                ></textarea>
+              </div>
+
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" disabled={saving} onClick={handleSave}>
+                  {saving ? "Saving..." : "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <LoaderIcon className="animate-spin size-10" /> */}
+    </div>
   )
 }
 
